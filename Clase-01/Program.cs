30b34c621bd9974d85bb9d6701e6c3f903fa3bef
camilaporro <camilaporro00@gmail.com>
2023-04-05 22:29:55 -0300

    Clase-01

---
+++ Clase-01/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Clase_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1: Ingresar 5 números por consola, guardándolos en una variable escalar.
+            //Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.
+
+            Console.Title = "Ejercicio 1 - Camila Porro";
+
+            int numero = 0;
+            int maximo = 0;
+            int minimo = 0;
+            int acumulador = 0;
+            int contador;
+            string? numeroIngresado;
+            float promedio = 0f;
+            int intentos = 5;
+
+            for (contador = 0; contador < intentos; contador++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                numeroIngresado = Console.ReadLine();
+
+                while (!(int.TryParse(numeroIngresado, out numero)))
+                {
+                    Console.WriteLine("Hubo un error. Reingrese el numero: ");
+                    numeroIngresado = Console.ReadLine();
+                }
+
+                acumulador += numero;
+
+                if (contador == 0)
+                {
+                    minimo = numero;
+                    maximo = numero;
+                }
+                else
+                {
+                    if (numero > maximo)
+                    {
+                        maximo = numero;
+                    }
+                    else
+                    {
+                        if (numero < minimo)
+                        {
+                            minimo = numero;
+                        }
+                    }
+                }
+
+            }
+
+            promedio = ((float)acumulador/ intentos);
+            Console.WriteLine("El numero mayor fue {0} y el menor {1}", maximo, minimo);
+            Console.WriteLine("Promedio: {0}", promedio);
+            Console.ReadKey();
+
+
+        }
+    }
+}
