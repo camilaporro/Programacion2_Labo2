a61d796d4df2313ac449b639af950005b7d75766
camilaporro <camilaporro00@gmail.com>
2023-04-22 19:52:45 -0300

    ejercicio 16

---
+++ Ejercicios/Ejercicio16/Alumno.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    internal class Alumno
+    {
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+
+        public string apellido;
+        public string nombre;
+        public int legajo;
+
+        public Alumno(string apellido, string nombre, int legajo)
+        {
+            this.apellido = apellido;
+            this.nombre = nombre;
+            this.legajo = legajo;
+        }
+
+        public void CalcularFinal()
+        {
+            if(nota1 > 4 && nota2 > 4) 
+            {
+                Random numeroRandom = new Random();
+                this.notaFinal = numeroRandom.Next(nota1, nota2);
+            }
+            else { this.notaFinal = -1; }
+        }
+
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            this.nota1 = notaUno;
+            this.nota2 = notaDos;
+        }
+
+        public string Mostrar()
+        {
+            string retornoAux = $"Alumno/a: {this.apellido}, {this.nombre} -- Legajo: {this.legajo} ";
+
+            if(this.notaFinal != -1)
+            {
+                retornoAux += $"-- La nota final es {this.notaFinal}";
+            }
+            else { retornoAux += "-- Alumno/a Desaprobado/a";  }
+
+            return retornoAux;
+        }
+        
+    }
+}
---
+++ Ejercicios/Ejercicio16/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Ejercicio16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno[] alumnos = new Alumno[3];
+            int legajo;
+            byte notaUno;
+            byte notaDos;
+
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                Console.Write("\nIngrese el apellido del alumno/a: ");
+                string? apellido = Console.ReadLine();
+
+                Console.Write("Ingrese el nombre del alumno/a: ");
+                string? nombre = Console.ReadLine();
+
+                Console.Write("Ingrese el numero de legajo del alumno/a: ");
+                int.TryParse(Console.ReadLine(), out legajo);
+
+                alumnos[i] = new Alumno(apellido, nombre, legajo);
+
+                Console.Write($"Ingrese la primer nota de {apellido}, {nombre}: ");
+                byte.TryParse(Console.ReadLine(), out notaUno);
+
+                Console.Write($"Ingrese la segunda nota de {apellido}, {nombre}: ");
+                byte.TryParse(Console.ReadLine(), out notaDos);
+
+                alumnos[i].Estudiar(notaUno, notaDos);
+            }
+
+            foreach (Alumno alumno in alumnos)
+            {
+                alumno.CalcularFinal();
+                Console.WriteLine("\n"+alumno.Mostrar());
+            }
+        }
+    }
+}
