c2fc64c5961ea196bceeb220dbcadb3c806d7265
camilaporro <camilaporro00@gmail.com>
2023-04-22 15:24:22 -0300

    ejercicio 6

---
+++ Ejercicios/Ejercicio6/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ejercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startYear;
+            int endingYear;
+
+
+            static bool IsLeapYear(int year)
+            {
+                if(year % 4 == 0 && (year % 100 !=0 || year % 400 == 0))
+                {
+                    return true;
+                }
+                else { return false; }
+            }
+
+            Console.WriteLine("Ingrese el año de inicio: ");
+            int.TryParse(Console.ReadLine(), out startYear);
+            Console.WriteLine("Ingrese el año de finalizacion: ");
+            int.TryParse(Console.ReadLine(), out endingYear);
+
+            Console.WriteLine($"Los años bisiestos entre {startYear} y {endingYear} son: ");
+
+            Console.WriteLine(IsLeapYear(startYear));
+
+            
+            for (int year = startYear; year <= endingYear; year++)
+            {
+                if(IsLeapYear(year)) { Console.WriteLine(year); }
+            }
+
+        }
+    }
+}
