e28401a5e769097c5edf2c44cc3355e620fe5f0a
camilaporro <camilaporro00@gmail.com>
2023-04-22 19:04:23 -0300

    ejercicio 15 completo

--- Ejercicios/Ejercicio15/Program.cs
+++ Ejercicios/Ejercicio15/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double numero1;
+            double numero2;
+            double resultado;
+            char operacion;
+            bool continuarCarga;
+
+
+            do
+            {
+                Console.Write("Ingrese el primer numero: ");
+                double.TryParse(Console.ReadLine(), out numero1);
+
+                Console.Write("\nIngrese el segundo numero: ");
+                double.TryParse(Console.ReadLine(), out numero2);
+
+                Console.Write("\nIngrese el la operacion (+, -, * o /): ");
+                char.TryParse(Console.ReadLine(), out operacion);
+
+                resultado = Calculadora.Calcular(numero1, numero2, operacion);
+
+                Console.WriteLine($"\n\nEl resultado de la operacion es {resultado}");
+
+                Console.WriteLine("\n\nDesea realizar otra operacion? (si - no)");
+                string respuesta = Console.ReadLine();
+                continuarCarga = respuesta == "si";
+            }
+            while(continuarCarga);
+
+            
+
         }
     }
 }
