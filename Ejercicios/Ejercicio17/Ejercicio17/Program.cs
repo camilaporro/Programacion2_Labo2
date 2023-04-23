78be31a94f310df60b05de7f11a971a255d50b76
camilaporro <camilaporro00@gmail.com>
2023-04-23 20:54:15 -0300

    ejercicio 17

---
+++ Ejercicios/Ejercicio17/Ejercicio17/Program.cs
@@ -0,0 +1,31 @@
+﻿using Entidades;
+
+namespace Ejercicio17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo boligrafoAzul = new Boligrafo(ConsoleColor.Blue, 100);
+            Boligrafo boligrafoRojo = new Boligrafo(ConsoleColor.Red, 50);
+
+            string dibujoAzul;
+            string dibujoRojo;
+                    
+            boligrafoAzul.Pintar(50, out dibujoAzul);
+            Console.ForegroundColor = boligrafoAzul.GetColor();
+            Console.WriteLine(dibujoAzul);
+                       
+            boligrafoRojo.Pintar(50, out dibujoRojo);
+            Console.ForegroundColor = boligrafoRojo.GetColor();
+            Console.WriteLine(dibujoRojo);
+
+            boligrafoRojo.Pintar(10, out dibujoRojo);
+            Console.ForegroundColor = boligrafoRojo.GetColor();
+            Console.WriteLine(dibujoRojo);
+
+
+
+        }
+    }
+}
---
+++ Ejercicios/Ejercicio17/Entidades/Boligrafo.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+
+namespace Entidades
+{
+    public class Boligrafo
+    {
+        public const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(ConsoleColor color, short tinta)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta() 
+        {
+            return this.tinta;
+        }
+
+        public bool Pintar(short gasto, out string dibujo)
+        {
+         
+            short tintaGastada = 0;
+            string dibujoAux = "";
+            bool retornoAux;
+
+            
+            if(this.tinta == 0) 
+            { 
+                dibujo = "No hay tinta. No se pudo pintar";
+                retornoAux = false;
+            }
+            else
+            {
+                while(this.tinta > 0 && tintaGastada < gasto) 
+                {
+                    dibujoAux += "*";
+                    tintaGastada++;
+                    this.SetTinta(-1);
+                }
+
+                dibujo = dibujoAux;
+                retornoAux = true;
+            }
+
+            return retornoAux;
+
+        }
+
+        public void Recargar()
+        {
+            this.SetTinta(cantidadTintaMaxima);
+        }
+
+        private void SetTinta(short tinta) 
+        {
+ 
+            if(tinta > 0) { this.CargarTinta(tinta); }
+            else { this.GastarTinta(tinta); }
+
+        }
+
+        private void CargarTinta(short tinta)
+        {
+            if(this.tinta + tinta > cantidadTintaMaxima) { this.tinta = cantidadTintaMaxima;}
+            else { this.tinta += tinta;}
+            
+        }
+
+        private void GastarTinta(short tinta)
+        {
+            //tinta es un numero negativo, por regla de signos se suma
+            //this.tinta =50; tinta = -10 (50 + (-10) = 40) --> esta ok
+            //this.tinta =50; tinta = -10 (50 - (-10) = 60) --> esta mal, no gasta tinta
+            if (this.tinta + tinta < 0) { this.tinta = 0; }
+            else { this.tinta += tinta; }
+            
+        }
+
+    }
+}
