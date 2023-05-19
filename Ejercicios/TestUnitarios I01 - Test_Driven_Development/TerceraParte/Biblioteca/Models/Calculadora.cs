b68cf4c9582375f5d90c32eba94d141c626a5305
camilaporro <camilaporro00@gmail.com>
2023-05-18 21:07:59 -0300

    TU I01 - Parte 3

---
+++ Ejercicios/TestUnitarios I01 - Test_Driven_Development/TerceraParte/Biblioteca/Models/Calculadora.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Models
+{
+    public class Calculadora
+    {
+
+        public int Add(string numeros)
+        {
+            int returnAux = 0;
+            int resultado = 0;
+
+
+            if (numeros.Contains(','))
+            {
+                string[] elementos = numeros.Split(',','\n');
+
+                foreach (string numero in elementos)
+                {
+                    resultado += int.Parse(numero);
+                }
+
+                returnAux = resultado;
+
+            }
+            else
+            {
+                int.TryParse(numeros, out resultado);
+                returnAux = resultado;
+            }
+
+
+            return returnAux;
+                
+        }
+    }
+}
