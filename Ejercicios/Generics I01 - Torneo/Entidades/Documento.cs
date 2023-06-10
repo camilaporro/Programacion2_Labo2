469077136bf5dd5f115a617b52bcafdae9cb0b1e
camilaporro <camilaporro00@gmail.com>
2023-06-10 15:14:46 -0300

    generics ej 1

---
+++ Ejercicios/Generics I01 - Torneo/Entidades/Documento.cs
@@ -0,0 +1,11 @@
+﻿namespace Entidades
+{
+    public class Documento
+    {
+        private int numero;
+
+        public Documento(int numero) { this.numero = numero; }
+
+        public int Numero { get => numero;}
+    }
+}
